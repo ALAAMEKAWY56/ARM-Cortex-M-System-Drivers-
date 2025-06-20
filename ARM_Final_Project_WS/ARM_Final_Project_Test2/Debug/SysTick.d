7a776497a690426c7e5feeb2015adb503db91632
ALAAMEKAWY56 <96634105+ALAAMEKAWY56@users.noreply.github.com>
2025-06-21 02:13:10 +0300

    Add files via upload

---
+++ ARM_Final_Project_WS/ARM_Final_Project_Test2/Debug/SysTick.d
@@ -0,0 +1,27 @@
+# FIXED
+
+SysTick.obj: ../SysTick.c
+SysTick.obj: ../std_types.h
+SysTick.obj: ../tm4c123gh6pm_registers.h
+SysTick.obj: ../SysTick.h
+SysTick.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h
+SysTick.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+SysTick.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+SysTick.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+
+../SysTick.c:
+
+../std_types.h:
+
+../tm4c123gh6pm_registers.h:
+
+../SysTick.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdlib.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
